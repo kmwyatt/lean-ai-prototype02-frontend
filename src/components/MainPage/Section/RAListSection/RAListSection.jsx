220b2e4d5dfdbed365ba4d9d14b4170b2051bf07
kmwyatt <arc2437@naver.com>
2022-03-09 21:24:01 +0900

    Modify RAListSection

--- src/components/MainPage/Section/RAListSection/RAListSection.jsx
+++ src/components/MainPage/Section/RAListSection/RAListSection.jsx
@@ -2,7 +2,12 @@
 
 import {
   Box,
+  Checkbox,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
   Paper,
+  Select,
   Table,
   TableBody,
   TableCell,
@@ -17,20 +22,127 @@ import SectionTitle from '../../../util/SectionTitle'
 import RAListTableRow from './RAListTableRow'
 
 function RAListSection() {
-  const [list, setList] = useState([])
+  const [showList, setShowList] = useState([])
+  // @ts-ignore
+  const [sortType, setSortType] = useState(0)
+  const [worker, setWorker] = useState(-1)
+  const [checker, setChecker] = useState(-1)
 
   async function getList() {
     const res = await axios.get('/api/admin/ralist')
-    setList(res.data)
+    setShowList(res.data)
+    setSortType(1)
   }
 
   useEffect(() => {
     getList()
   }, [])
 
+  useEffect(() => {
+    console.log('showList updated')
+  }, [showList])
+
+  // @ts-ignore
+  const workerHandler = (event) => {
+    if (event.target.checked) {
+      setWorker(-1)
+    } else {
+      setWorker(1)
+    }
+  }
+
+  // @ts-ignore
+  const checkerHandler = (event) => {
+    if (event.target.checked) {
+      setChecker(-1)
+    } else {
+      setChecker(2)
+    }
+  }
+
+  // @ts-ignore
+  const selectHandler = (event) => {
+    setSortType(event.target.value)
+    // @ts-ignore
+    let updateList = showList
+    console.log('updateList', updateList)
+
+    if (event.target.value === 1) {
+      // @ts-ignore
+      updateList.sort((a, b) => {
+        if (a.index < b.index) {
+          return 1
+        }
+        if (a.index > b.index) {
+          return -1
+        }
+      })
+    } else if (event.target.value === 2) {
+      // @ts-ignore
+      updateList.sort((a, b) => {
+        if (a.index < b.index) {
+          return -1
+        }
+        if (a.index > b.index) {
+          return 1
+        }
+      })
+    } else if (event.target.value === 3) {
+      // @ts-ignore
+      updateList.sort((a, b) => {
+        if (a.workNum < b.workNum) {
+          return 1
+        }
+        if (a.workNum > b.workNum) {
+          return -1
+        }
+      })
+    } else if (event.target.value === 4) {
+      // @ts-ignore
+      updateList.sort((a, b) => {
+        if (a.workNum < b.workNum) {
+          return -1
+        }
+        if (a.workNum > b.workNum) {
+          return 1
+        }
+      })
+    }
+    // @ts-ignore
+    setShowList(updateList)
+  }
+
   return (
     <Box sx={{ m: 5, width: '100%' }}>
       <SectionTitle text="RA 관리" />
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'right',
+          mb: 2,
+        }}
+      >
+        <Box sx={{ mr: 2 }}>
+          <FormControlLabel
+            control={<Checkbox defaultChecked size="small" />}
+            onChange={workerHandler}
+            label="작업 RA"
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked size="small" />}
+            onChange={checkerHandler}
+            label="검수 RA"
+          />
+        </Box>
+        <InputLabel>정렬 기준&nbsp;&nbsp;</InputLabel>
+        <Select size="small" defaultValue={1} onChange={selectHandler}>
+          <MenuItem value={1}>가입일자 ↓</MenuItem>
+          <MenuItem value={2}>가입일자 ↑</MenuItem>
+          <MenuItem value={3}>작업량 ↓</MenuItem>
+          <MenuItem value={4}>작업량 ↑</MenuItem>
+        </Select>
+      </Box>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
@@ -45,9 +157,12 @@ function RAListSection() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {list.map((ra) => (
-              <RAListTableRow info={ra} />
-            ))}
+            {showList
+              // @ts-ignore
+              .filter((ra) => ra.role !== worker && ra.role !== checker)
+              .map((ra) => (
+                <RAListTableRow info={ra} />
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
